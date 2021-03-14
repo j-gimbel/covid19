e7bc683695843c984c1ba1078a3130f30f0773ad
Jan Gimbel <github-4597@jgimbel.de>
2021-03-15 00:16:07 +0100

    improved response model

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Bundesland_Base(BaseModel):
@@ -33,13 +33,13 @@ class GeoDemo_Base(BaseModel):
     Einwohner: int
     Bundesland: str
 
-    AnzahlFallNeu_7_Tage_Dropped: int
-    AnzahlTodesfallNeu_7_Tage: int
-    Fallsterblichkeit_Prozent: float
+    AnzahlFallNeu_7_Tage_Dropped: int = Field(alias="AnzahlFallNeu-7-Tage-Dropped")
+    AnzahlTodesfallNeu_7_Tage: int = Field(alias="AnzahlFallNeu-7-Tage-Dropped")
+    Fallsterblichkeit_Prozent: float = Field(alias="Fallsterblichkeit-Prozent")
     Kontaktrisiko: float
-    InzidenzFallNeu_7_Tage: float
-    MeldeDauerFallNeu_Schnitt: float
-    InzidenzFallNeu_7_Tage_Trend_Spezial: float
+    InzidenzFallNeu_7_Tage: float = Field(alias="InzidenzFallNeu-7-Tage")
+    MeldeDauerFallNeu_Schnitt: float = Field(alias="MeldeDauerFallNeu-Schnitt")
+    InzidenzFallNeu_7_Tage_Trend_Spezial: float = Field(alias="InzidenzFallNeu-7-Tage-Trend-Spezial")
     Kehrwert_risiko: float
 
     class Config:
