3c6d9e27ea53f4c00c5d18c6d39c0d6ce7af4ba1
Jan Gimbel <github-4597@jgimbel.de>
2021-03-25 23:31:55 +0100

    progess on charts

--- geojson/map/src/components/Map.vue
+++ geojson/map/src/components/Map.vue
@@ -1,8 +1,8 @@
 <template>
-  <div v-if="geoJsonloaded" class="container-fluid vh-100">
+  <div class="container-fluid vh-100">
     <div class="row vh-100">
-      <div class="col-9">
-        <l-map :zoom="7" :center="[51.4, 9.0]">
+      <div class="col-7">
+        <l-map :zoom="7" :center="[51.4, 10.4]">
           <l-geo-json
             v-if="geoJsonloaded"
             :geojson="geojson"
@@ -14,10 +14,14 @@
         </l-map>
       </div>
 
-      <div class="col-3">
+      <div class="col-5">
         <h3>Info</h3>
-        Landkreis:
-        <span> {{ lkName }}</span>
+        <div>
+          Landkreis:
+
+          <span> {{ landkreisName }}</span>
+          <div v-show="landkreisId" id="chart1"></div>
+        </div>
       </div>
     </div>
   </div>
@@ -25,17 +29,43 @@
 <script>
 import { ref, onMounted, watch, computed, toRefs } from "vue";
 import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
-/*import {
-  LMap,
-  LTileLayer,
-  LGeoJson,
-} from "@vue-leaflet/vue-leaflet/src/components";*/
+
+// import { Plotly } from "vue-plotly";
+
+//import Plotly from "plotly.js";
+//var Plotly = require("plotly.js-dist");
 import "leaflet/dist/leaflet.css";
 export default {
   components: {
     LMap,
     LTileLayer,
     LGeoJson,
+    Plotly,
+  },
+
+  mounted() {
+    console.log("mounted");
+    /*
+    let plotlyScript = document.createElement("script");
+    plotlyScript.setAttribute(
+      "src",
+      "https://cdn.plot.ly/plotly-latest.min.js"
+    );
+    document.head.appendChild(recaptchaScript);*/
+    /*var TESTER = document.getElementById("tester");
+    console.log(TESTER);
+    window.Plotly.newPlot(
+      TESTER,
+      [
+        {
+          x: [1, 2, 3, 4, 5],
+          y: [1, 2, 4, 8, 16],
+        },
+      ],
+      {
+        margin: { t: 0 },
+      }
+    );*/
   },
 
   setup(props) {
@@ -59,28 +89,71 @@ export default {
     const selectedLayer = ref(null);
     const hoveredLayer = ref(null);
     */
-    landkreisName = ref("");
+
     var selectedLayer = null;
     var hoveredLayer = null;
+
+    const landkreisId = ref(null);
+    const landkreisName = ref(null);
+    const chart1Data = ref({});
     const geoJsonloaded = ref(false);
     const geojson = ref({});
 
     // load new data when landkreisId changes
-    const landkreisId = ref(null);
+
     const getLandKreisData = async () => {
-      console.log(landkreisId.value);
-      fetch("http://localhost:8000/api/bundeslaender")
+      console.log("getLandKreisData", landkreisId.value);
+      fetch(
+        "http://j-gimbel.ddns.net:8000/api/landkreis/" + landkreisId.value,
+        {
+          mode: "cors",
+        }
+      )
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          landkreisName = data.name;
+          //landkreisName.value = data.name;
+          chart1Data.value = data;
         });
     };
     watch(landkreisId, getLandKreisData);
 
-    const getLKData = async () => {
-      fetch();
+    const updateChart1 = () => {
+      var data = chart1Data.value;
+      console.log(
+        "updateChart1",
+        chart1Data.value.Einwohner_AG_A00_A04,
+        data.Einwohner_AG_A00_A04
+      );
+      window.Plotly.react(
+        document.getElementById("chart1"),
+        [
+          {
+            x: [
+              "AG 0-4",
+              "AG 5-14",
+              "AG 15-34",
+              "AG 35-59",
+              "AG 60-79",
+              "AG 80+",
+            ],
+            y: [
+              data.Einwohner_AG_A00_A04,
+              data.Einwohner_AG_A05_A14,
+              data.Einwohner_AG_A15_A34,
+              data.Einwohner_AG_A35_A59,
+              data.Einwohner_AG_A60_A79,
+              data.Einwohner_AG_A80Plus,
+            ],
+            type: "bar",
+          },
+        ],
+        { title: "Age groups distribution" },
+        { responsive: true }
+      );
     };
+
+    watch(chart1Data, updateChart1);
 
     const getGeojson = async () => {
       var myHeaders = new Headers();
@@ -101,12 +174,33 @@ export default {
           geojson.value = geojson_data;
           geoJsonloaded.value = true;
 
-          return geojson_data;
+          window.Plotly.newPlot(
+            document.getElementById("chart1"),
+            //date
+            [
+              {
+                x: [
+                  "AG 0-4",
+                  "AG 5-14",
+                  "AG 15-34",
+                  "AG 35-59",
+                  "AG 60-79",
+                  "AG 80+",
+                ],
+                y: [0, 0, 0, 0, 0, 0],
+                type: "bar",
+              },
+            ],
+            //layout
+            { title: "Age groups distribution" },
+            //config
+            { responsive: true }
+          );
         });
     };
 
     const whenClicked = (e) => {
-      //console.log("whenClicked", e);
+      console.log("whenClicked", e);
       var layer = e.target;
 
       if (selectedLayer != null) {
@@ -126,9 +220,11 @@ export default {
     };
 
     const whenMouseover = (e) => {
-      //console.log("whenMouseover", e);
+      console.log("whenMouseover", e);
       var layer = e.target;
       hoveredLayer = layer;
+
+      // this is just to not change the style if the user hover the selected layer
       if (selectedLayer != null) {
         if (
           selectedLayer.feature.properties.id == layer.feature.properties.id
@@ -136,14 +232,15 @@ export default {
           return;
         }
       }
+      landkreisName.value = hoveredLayer.feature.properties.name;
       layer.setStyle(hoverStyle);
     };
 
     const whenMouseout = (e) => {
-      console.log("whenMouseout", e);
+      //console.log("whenMouseout", e);
       var layer = e.target;
+      // this is just to not change the style if the user hover the selected layer
       if (selectedLayer != null) {
-        console.log(selectedLayer);
         if (
           selectedLayer.feature.properties.id == layer.feature.properties.id
         ) {
@@ -171,14 +268,15 @@ export default {
 
     return {
       geojson,
-      getGeojson,
-      onEachFeature,
-      setGeoJsonStyle,
-      whenClicked,
-      selectedLayer,
-      hoveredLayer,
+      // getGeojson,
+      // onEachFeature,
+      // setGeoJsonStyle,
+      // whenClicked,
+      // selectedLayer,
+      // hoveredLayer,
       geoJsonloaded,
-      lkName,
+      landkreisName,
+      landkreisId,
       geojsonOptions: {
         onEachFeature: onEachFeature,
         style: setGeoJsonStyle,
